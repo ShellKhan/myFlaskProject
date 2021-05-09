6f9c994c91235637e5de28d7c344f8fc182ead58
Jorian <jorian@mail.ru>
2021-05-09 22:44:28 +0300

    homework 2 addition - alternate start method

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-python wsgi.py
+:: python wsgi.py
+flask run
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 from blog.app import create_app
 
 app = create_app()
-if __name__ == "__main__":
-    app.run(
-        host="localhost",
-        port=8080,
-        debug=True,
-    )
+# if __name__ == "__main__":
+#     app.run(
+#         host="localhost",
+#         port=8080,
+#         debug=True,
+#     )
