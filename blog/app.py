6d0cc5e5239c0ed38660018a9020d72d4db60e6f
Jorian <jorian@mail.ru>
2021-05-23 03:01:55 +0300

    homework 3 stage 5

--- blog/app.py
+++ blog/app.py
@@ -6,8 +6,11 @@ from . import commands
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = "18349276"
+    # app.config["ENV"] = 'development'
+    # app.config["DEBUG"] = True
 
     @app.route("/")
     def index():
