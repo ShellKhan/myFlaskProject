6c21984a3dbdc0966435ee8f763490f9d0d5d96b
jorian <jorian@mail.ru>
2021-05-29 00:38:07 +0300

    homework 4 stage 1 add configs

--- blog/app.py
+++ blog/app.py
@@ -6,11 +6,7 @@ from . import commands
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SECRET_KEY"] = "18349276"
-    # app.config["ENV"] = 'development'
-    # app.config["DEBUG"] = True
+    app.config.from_object('blog.configs')
 
     @app.route("/")
     def index():
