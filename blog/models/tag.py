e7cd4f114dbeb0a822875746f19bb02e8c495678
Jorian <jorian@mail.ru>
2021-06-29 00:25:30 +0300

    homework 7 stage 5

--- blog/models/tag.py
+++ blog/models/tag.py
@@ -8,5 +8,6 @@ class Tag(db.Model):
     __tablename__ = 'tags'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), nullable=False, default="", server_default="")
-    articles = relationship("Article", secondary=article_tag_association_table, back_populates="tags")
+    name = db.Column(db.String(255), nullable=False, default='', server_default='')
+
+    articles = relationship('Article', secondary=article_tag_association_table, back_populates='tags')
