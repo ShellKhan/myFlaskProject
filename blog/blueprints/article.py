b06e6391ac7c268ce9f37583287d2c55f83c0bb9
Jorian <jorian@mail.ru>
2021-06-29 22:51:08 +0300

    homework 7 stage 9

--- blog/blueprints/article.py
+++ blog/blueprints/article.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
+from sqlalchemy.orm import joinedload
 from werkzeug.exceptions import NotFound
 
 from blog.instruments import db
 from blog.forms import CreateArticleForm
-from blog.models import Author, Article
+from blog.models import Author, Article, Tag
 
 article = Blueprint(
     name='article',
@@ -23,9 +24,19 @@ def article_list():
     )
 
 
+@article.route('/tags/<int:tag_id>/', methods=['GET'])
+def article_tag_list(tag_id):
+    tag = Tag.query.filter_by(id=tag_id).options(joinedload(Tag.articles)).one_or_none()
+    return render_template(
+        'articles/list.html',
+        articles=tag.articles,
+        tag=tag
+    )
+
+
 @article.route('/<int:article_id>/', methods=['GET'])
 def article_detail(article_id):
-    _article = Article.query.filter_by(id=article_id).one_or_none()
+    _article = Article.query.filter_by(id=article_id).options(joinedload(Article.tags)).one_or_none()
     if _article is None:
         raise NotFound
     return render_template(
@@ -38,8 +49,11 @@ def article_detail(article_id):
 @login_required
 def create_article():
     form = CreateArticleForm(request.form)
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by("name")]
+
     if request.method == "POST" and form.validate_on_submit():
         _article = Article(title=form.title.data.strip(), text=form.text.data)
+
         if current_user.author:
             _article.author_id = current_user.author.id
         else:
@@ -47,6 +61,11 @@ def create_article():
             db.session.add(author)
             db.session.flush()
             _article.author_id = author.id
+
+        if form.tags.data:
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+            for tag in selected_tags:
+                _article.tags.append(tag)
 
         db.session.add(_article)
         db.session.commit()
